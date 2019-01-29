d4447c08aa2f2f99fa25fe0992cc991aed9fa6ab
luobotang <tangkangxing@sina.com>
2019-01-29 10:54:14 +0800

    support column auto completion

--- app/index.js
+++ app/index.js
@@ -1,5 +1,37 @@
 require.config({ paths: { 'vs': '../node_modules/monaco-editor/min/vs' }})
 require(['vs/editor/editor.main'], function() {
+  const SORT_TEXT = {
+    Table: '0',
+    Column: '1',
+    Keyword: '2'
+  }
+  const db_keywords = [
+    'SELECT',
+    'FROM',
+    'WHERE',
+    'GROUP BY',
+    'ORDER BY',
+    'ASC',
+    'DESC'
+  ]
+  const db_schema = window.db_schema = {
+    tables: [
+      'tb_user',
+      'tb_log'
+    ],
+    table_columns: {
+      tb_user: [
+        {name: 'user_id', type: 'varchar'},
+        {name: 'user_name', type: 'varchar'}
+      ],
+      tb_log: [
+        {name: 'log_id', type: 'varchar'},
+        {name: 'log_create_time', type: 'varchar'},
+        {name: 'log_type', type: 'varchar'},
+        {name: 'log_content', type: 'varchar'},
+      ]
+    }
+  }
   window.editor = monaco.editor.create(document.getElementById('container'), {
     value: 'select * from tb_user;',
     language: 'sql',
@@ -10,7 +42,7 @@ require(['vs/editor/editor.main'], function() {
     suggestOnTriggerCharacters: true
   });
   monaco.languages.registerCompletionItemProvider('sql', {
-    triggerCharacters: [' '],
+    triggerCharacters: [' ', '.'],
     provideCompletionItems(model, position) {
       const {lineNumber, column} = position
       const textBeforePointer = model.getValueInRange({
@@ -20,27 +52,61 @@ require(['vs/editor/editor.main'], function() {
         endColumn: column
       })
       const tokens = textBeforePointer.trim().split(/\s+/)
-      if (tokens[tokens.length - 1] === 'from') {
+      const lastToken = tokens[tokens.length - 1].toLowerCase()
+      if (lastToken === 'from') {
         return {
-          suggestions: [
-            'tb_user',
-            'tb_platform',
-            'tb_log',
-            'tb_connections'
-          ].map(renderTable)
+          suggestions: db_schema.tables.map(renderTable)
+        }
+      } else if (lastToken === 'select') {
+        return {
+          suggestions: getAllTableColumnCompletionItems()
         }
       }
-      return {suggestions: []}
+      return {
+        suggestions: db_schema.tables.map(renderTable)
+          .concat(getAllTableColumnCompletionItems())
+          .concat(db_keywords.map(renderKeyword))
+      }
     }
   })
+
+  function getAllTableColumnCompletionItems() {
+    const table_columns = []
+    Object.keys(db_schema.table_columns).forEach(table => {
+      db_schema.table_columns[table].forEach(column => {
+        table_columns.push(renderTableColumn(table, column))
+      })
+    })
+    return table_columns
+  }
+
+  function renderKeyword(keyword) {
+    return {
+      label: keyword,
+      kind: monaco.languages.CompletionItemKind.Keyword,
+      detail: '',
+      sortText: SORT_TEXT.Keyword,
+      insertText: keyword
+    }
+  }
 
   function renderTable(name) {
     return {
       label: name,
-      kind: monaco.languages.CompletionItemKind.Text,
-      detail: name,
-      documentation: name,
+      kind: monaco.languages.CompletionItemKind.Module,
+      detail: '<table>',
+      sortText: SORT_TEXT.Table,
       insertText: name
+    }
+  }
+
+  function renderTableColumn(table, column) {
+    return {
+      label: column.name,
+      kind: monaco.languages.CompletionItemKind.Field,
+      detail: '<column> ' + column.type + ' ' + table,
+      sortText: SORT_TEXT.Column,
+      insertText: column.name
     }
   }
 });
